0a0ef168835ed09650c0f9c14088ee2286e943e8
Rajnish <rajnishtripathi2001@gmail.com>
2023-02-15 22:02:38 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState,React} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const Increment = ()=>{
+    if (counter < 10) {
+      setCounter(counter+1)
+    }
+  }
+
+  const Decrement = ()=>{
+    if (counter > 0) {
+      setCounter(counter-1)
+    }
+  }
+
+  useEffect(() => {
+    const color=document.getElementById('count-text')
+    if (counter<=4) {
+      color.style.color='green'
+
+    }
+    else if (counter>4 && counter<=9) {
+      color.style.color='blue'
+    }
+    else{
+      color.style.color='red'
+    }
+
+  }, [counter])
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>A Counter Application which Count from 0 to 10 <br/>
+      and <br/>change color from 0-4 (Green) , 5-9(Blue) , 10 (Red)</h2>
+    <div className='counter-container'>
+      <h1>Count</h1>
+      <h1 id='count-text'>{counter}</h1>
+      
+      <div className='btn-box'>
+        <button id='inc' onClick={Increment}>Increment</button>
+        <button id='dec' onClick={Decrement}>Decrement</button>
+      </div>
     </div>
+    <p>Made By <em><strong>Rajnish Tripathi</strong></em> - 12011284</p>
+  </div>
   );
 }
 
